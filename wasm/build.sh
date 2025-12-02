901e43e0d219427fed4e8b3c2e94c0cb4d12ba80
BayernMuller <cjjunk29@gmail.com>
2025-12-02 20:08:16 +0900

    feat: integrate WASM-based emotion recognition and update UI for emotion display

--- wasm/build.sh
+++ wasm/build.sh
@@ -2,9 +2,11 @@
 
 emcc main.cpp geometry.cpp predict.cpp -o expression_recognition.js \
   -s EXPORTED_FUNCTIONS='["_process_frame", "_malloc", "_free"]' \
-  -s EXPORTED_RUNTIME_METHODS='["ccall", "HEAPU8", "HEAPF32"]' \
+  -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "HEAPU8", "HEAPF32"]' \
   -s ALLOW_MEMORY_GROWTH=1 \
-  -O3
+  -s WASM=1 \
+  -s MODULARIZE=1 \
+  -s EXPORT_NAME='createExpressionRecognitionModule'
 
 if [ $? -ne 0 ]; then
     echo "Failed to build expression_recognition.js"
